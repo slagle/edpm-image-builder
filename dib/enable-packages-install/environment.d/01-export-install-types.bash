4ee5f17331e852ab8b2315649019ef3e59a084e5
Jiri Podivin <jpodivin@redhat.com>
2023-03-28 16:26:46 +0200

    Bashate settings and fixes to various scripts
    Indentation was harmonized across the repo. Scripts without shebang have been given one,
    the optional `()` in function definitions has been dropped.
    
    Signed-off-by: Jiri Podivin <jpodivin@redhat.com>

--- dib/enable-packages-install/environment.d/01-export-install-types.bash
+++ dib/enable-packages-install/environment.d/01-export-install-types.bash
@@ -1,1 +1,2 @@
+#!/bin/bash
 export DIB_DEFAULT_INSTALLTYPE=package
--- dib/repo-setup/environment.d/01-repo-setup.bash
+++ dib/repo-setup/environment.d/01-repo-setup.bash
@@ -1,3 +1,4 @@
+#!/bin/bash
 export DIB_REPO_SETUP=${DIB_REPO_SETUP:-current-podified}
 export DIB_REPO_SETUP_BRANCH=${DIB_REPO_SETUP_BRANCH:-master}
 export DIB_REPO_SETUP_DISTRO_MIRROR=${DIB_REPO_SETUP_DISTRO_MIRROR:-''}
